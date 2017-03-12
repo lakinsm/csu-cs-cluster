5a7ed216d67451bbc7aa74c3fad0a73e25d74562
lakinsm <lakinsm@rams.colostate.edu>
2017-03-11 23:40:35 -0700

    11March

--- servers/11March2017_send_models.sh
+++ servers/11March2017_send_models.sh
@@ -1,1 +1,1 @@
-/s/chopin/a/grad/lakinsm/uftp-4.9.2/uftp /home/lakinsm/hmm_testing/cs_cluster_files/meta-marc/src/HMMs -E /home/lakinsm/hmm_testing/cs_cluster_files/meta-marc/src/HMMs -R 50000 -D HMMs
+/s/chopin/a/grad/lakinsm/uftp-4.9.2/uftp ~/meta-marc/src/HMMs -E ~/meta-marc/src/HMMs -R 50000 -D HMMs
